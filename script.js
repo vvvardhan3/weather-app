b3fe3103ef1b1a33c4ab7d6b9f63b124cf98394d
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2023-09-08 18:21:58 -0400

    Used weather api

---
+++ script.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("search-button");
+const input = document.getElementById("city-input");
+
+const cityName = document.getElementById('city-name');
+const cityTime = document.getElementById('city-time');
+const cityTemp = document.getElementById('city-temp');
+
+
+async function getData(cityName) {
+   const promise = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=bd6c4eb1e5314312ab8213531230809&q=${cityName}&aqi=yes`
+    );
+    return await promise.json()
+}
+
+button.addEventListener("click", async () => {
+    const value= input.value;
+    const result = await getData(value);
+    cityName.innerText = `${result.location.name},${result.location.region}-${result.location.country}`
+    console.log(result);
+    
+});
